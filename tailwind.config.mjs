032ab35ca13cfa06b41c015384ed8e9e2e299eed
Joan Pastillo <jeipige@gmail.com>
2026-01-18 15:08:40 -0500

    updt: add GSAP library and redesign UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,25 @@
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/@fortawesome/fontawesome-free/css/all.css", // Agregar aquí
+    "./node_modules/@fortawesome/fontawesome-free/css/all.css",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        playfair: ["PlayfairDisplay", "sans-serif"], // Reemplaza 'sans' si quieres usarla como fuente predeterminada
+      colors: {
+        'chocolate': '#2d1b14',
+        'terracotta': '#e07a5f',
+        'cream': '#fdfbf7',
+        'off-white': '#fafafa',
+        'soft-pink': '#eec0c6', // Soft pink as requested
       },
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        sans: ["Outfit", "sans-serif"], // Minimalist geometric sans
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+      }
     },
   },
   plugins: [],
